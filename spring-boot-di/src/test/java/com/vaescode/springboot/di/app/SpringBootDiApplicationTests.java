e490f63f08580ea4c8bd7de7107aca4d04533e2c
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-08 18:26:41 -0600

    Inicializando practica con inyección de dependencias

---
+++ spring-boot-di/src/test/java/com/vaescode/springboot/di/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaescode.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

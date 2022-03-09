2304fe1e38ff2c07fd0a2beb3b2bb3be6f639828
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-09 17:59:25 -0600

    @Value: validando formulario usando anotaciones

--- spring-boot-form/src/main/java/com/vaescode/springbootform/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/vaescode/springbootform/app/controllers/FormController.java
@@ -3,8 +3,14 @@ package com.vaescode.springbootform.app.controllers;
 import com.vaescode.springbootform.app.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class FormController {
@@ -16,7 +22,17 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesarFormulario(Usuario usuario, Model model) {
+    public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model) {
+
+
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
 
         model.addAttribute("titulo", "Resultado del formulario ");
         model.addAttribute("usuario", usuario);

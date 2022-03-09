42951a04fa804c2bd65f3de5f20b95766bafe10d
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-08 21:53:45 -0600

    @Qualifier: ejemplo de uso de otra opción de designar prioridad de inyección de bean

--- spring-boot-di/src/main/java/com/vaescode/springboot/di/app/controllers/IndexController.java
+++ spring-boot-di/src/main/java/com/vaescode/springboot/di/app/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.vaescode.springboot.di.app.controllers;
 
 import com.vaescode.springboot.di.app.models.service.IServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @Autowired
+    @Qualifier("miServicioSimple")
     private IServicio servicio;
 
     @GetMapping({"", "/", "/index"})
--- spring-boot-di/src/main/java/com/vaescode/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/vaescode/springboot/di/app/models/service/MiServicio.java
@@ -1,14 +1,16 @@
 package com.vaescode.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component("miServicioSimple")
+@Primary // Esta anotación permite definir que bean tiene prioridad de inyección en el controller
 public class MiServicio implements IServicio {
 
 
     @Override
     public String operacion() {
-        return "Ejecutando algún proceso importante usando una interfaz...";
+        return "Ejecutando algún proceso importante simple usando una interfaz...";
     }
 }

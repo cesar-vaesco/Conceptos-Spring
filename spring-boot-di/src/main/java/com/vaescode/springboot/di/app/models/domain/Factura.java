a4702fdacf9445e154a41173341950d05156d766
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-09 13:19:31 -0600

    Scope

--- spring-boot-di/src/main/java/com/vaescode/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/vaescode/springboot/di/app/models/domain/Factura.java
@@ -1,16 +1,18 @@
 package com.vaescode.springboot.di.app.models.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.Serializable;
 import java.util.List;
 
 @Component
-public class Factura {
+@RequestScope
+public class Factura implements Serializable{
 
     @Value("${factura.descripcion}")
     private String descripcion;

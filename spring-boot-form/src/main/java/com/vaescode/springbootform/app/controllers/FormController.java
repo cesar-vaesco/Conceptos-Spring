920066862327fef03077165f6be3c39b5b5a22bd
cesar-vaesco <cesarvargasescorcia@gmail.com>
2022-03-09 19:58:44 -0600

    Añadiendo mensajes de errores en la vista del formulario

--- spring-boot-form/src/main/java/com/vaescode/springbootform/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/vaescode/springbootform/app/controllers/FormController.java
@@ -1,28 +1,34 @@
 package com.vaescode.springbootform.app.controllers;
 
-import com.vaescode.springbootform.app.models.domain.Usuario;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import com.vaescode.springbootform.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+    	
+    	Usuario usuario  = new Usuario();
         model.addAttribute("titulo", "Formulario usuario ");
+        model.addAttribute("user", usuario);
         return "form";
     }
+    
 
     @PostMapping("/form")
-    public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model) {
+    public String procesarFormulario(@Valid @ModelAttribute(value = "user") Usuario usuario, BindingResult result, Model model) {
 
 
         if(result.hasErrors()){

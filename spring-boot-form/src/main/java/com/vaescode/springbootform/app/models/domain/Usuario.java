04c3cb9fe5a19f2b60762029a1a298ceef8a86f1
cesar-vaesco <cesarvargasescorcia@gmail.com>
2022-03-12 14:21:04 -0600

    Validacion: Validando que no ingresar espacios en blanco con @NotBlack

--- spring-boot-form/src/main/java/com/vaescode/springbootform/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/vaescode/springbootform/app/models/domain/Usuario.java
@@ -1,6 +1,7 @@
 package com.vaescode.springbootform.app.models.domain;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -16,7 +17,7 @@ public class Usuario {
 	@NotEmpty
 	private String apellido;
 
-	@NotEmpty
+	@NotBlank //Valida espacios en blanco 
 	@Size(min = 3, max = 8)
 	private String username;
 

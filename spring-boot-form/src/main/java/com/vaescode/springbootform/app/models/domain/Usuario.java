f9078ea699866cf32f38ed1a213fb0a5a1fca578
cesar-vaesco <cesarvargasescorcia@gmail.com>
2022-03-12 18:23:04 -0600

    Validación: Validando números enteros con @NotNull, @Max, @Min

--- spring-boot-form/src/main/java/com/vaescode/springbootform/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/vaescode/springbootform/app/models/domain/Usuario.java
@@ -1,8 +1,11 @@
 package com.vaescode.springbootform.app.models.domain;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.vaescode.springbootform.app.validation.IdentificadorRegex;
@@ -31,6 +34,11 @@ public class Usuario {
 	@Requerido
 	@Email
 	private String email;
+
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
 
 	public String getIdentificador() {
 		return identificador;
@@ -78,6 +86,14 @@ public class Usuario {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Integer getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
 	}
 
 }

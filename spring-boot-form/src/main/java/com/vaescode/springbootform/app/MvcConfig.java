257167d306f90e2c04ba51196a401897a4ee12af
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-14 12:24:09 -0600

    Interceptor: Validando la existencia de una variable para imprimir en thymeleaf

--- spring-boot-form/src/main/java/com/vaescode/springbootform/app/MvcConfig.java
+++ spring-boot-form/src/main/java/com/vaescode/springbootform/app/MvcConfig.java
@@ -17,7 +17,7 @@ public class MvcConfig  implements WebMvcConfigurer{
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(tiempoTranscurridoInterceptor);
+		registry.addInterceptor(tiempoTranscurridoInterceptor).addPathPatterns("/form/**");
 	}
 	
 	
